72752b7565b1a87aa7a51ca68d8e8b747f1b6923
Sérgio da Rocha <sergiorajr1994@gmail.com>
2024-07-15 11:20:41 -0300

    inseridas tabelas, rotas e etc às demais tabelas

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -13,6 +13,26 @@ class Controller {
         }
     }
 
+    async pegaUmPorId(req, res) {
+        const { id } = req.params;
+        try {
+            const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+            return res.status(200).json(umRegistro);
+        } catch (erro) {
+        // erro
+        }
+    }
+
+    async criaNovo(req, res) {
+        const dadosParaCriacao = req.body;
+        try {
+            const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+            return res.status(200).json(novoRegistroCriado);
+        } catch (erro) {
+        // erro
+        }
+    }
+
     async atualiza (req, res){
         const { id } = req.params;
         const dadosAtualizados = req.body;
@@ -24,6 +44,16 @@ class Controller {
             return res.status(200).json(({mensagem: 'Atualizando com sucesso'}));
         } catch (erro){
             //erro
+        }
+    }
+
+    async exclui(req, res) {
+        const { id } = req.params;
+        try {
+            await this.entidadeService.excluiRegistro(Number(id));
+            return res.status(200).json({ mensagem: `id ${id} deletado` });
+        } catch (error) {
+            return res.status(500).json(error.message);
         }
     }
 }
